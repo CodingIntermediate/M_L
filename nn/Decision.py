7a69bc61a60a5c3d0c7acc5d0dd0838c122d7108
Hari <133503078+CodingIntermediate@users.noreply.github.com>
2025-07-21 00:15:18 +0530

    Update Decision.py

--- nn/Decision.py
+++ nn/Decision.py
@@ -1,9 +1,10 @@
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
 from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
+import pandas as pd
 # Step 1: Load the dataset
 iris = load_iris()
 X = iris.data
@@ -25,8 +26,16 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy:{accuracy:.2f}")
 # Accuracy:1.00
+
+iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+# Add target column to DataFrame
+iris_df['target'] = iris.target
+# View the first few rows
+print(iris_df.head())
+
 plt.figure(figsize=(10,8))
 plot_tree(model, filled=True, feature_names=iris.feature_names, class_names=iris.target_names)
+print("features:",iris.feature_names)
 plt.title("Decision Tree Trained on Iris Dataset")
-plt.show()
+plt.show() 
 # (graph)
